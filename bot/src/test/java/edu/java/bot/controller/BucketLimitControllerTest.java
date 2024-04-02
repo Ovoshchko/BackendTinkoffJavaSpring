8c52c0eadbe873b87bdf95a74d7760f2a2fed661
Ovoshchko <ashinyakov@bk.ru>
2024-04-02 18:30:28 +0300

    Добавил фейк пропертис

--- bot/src/test/java/edu/java/bot/controller/BucketLimitControllerTest.java
+++ bot/src/test/java/edu/java/bot/controller/BucketLimitControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cache.CacheManager;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -18,6 +19,7 @@ import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@TestPropertySource(locations = "classpath:/nonexistent.properties")
 @SpringBootTest
 @DirtiesContext
 public class BucketLimitControllerTest {
