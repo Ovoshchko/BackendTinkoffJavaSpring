6513152d8abf9ce04c9771fce53b422faae2250c
Ovoshchko <ashinyakov@bk.ru>
2024-04-02 03:18:26 +0300

    падает тест

--- bot/src/test/java/edu/java/bot/controller/BucketLimitControllerTest.java
+++ bot/src/test/java/edu/java/bot/controller/BucketLimitControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cache.CacheManager;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@DirtiesContext
 public class BucketLimitControllerTest {
 
     public static final String UPDATES_URL = "/updates";
