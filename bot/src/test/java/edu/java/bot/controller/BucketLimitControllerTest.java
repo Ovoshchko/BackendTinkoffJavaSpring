9c645fb93335d656edeb78fa397ea3cfe08edc43
Ovoshchko <ashinyakov@bk.ru>
2024-04-14 23:20:23 +0300

    А вот и всплыл кривой тест)

--- bot/src/test/java/edu/java/bot/controller/BucketLimitControllerTest.java
+++ bot/src/test/java/edu/java/bot/controller/BucketLimitControllerTest.java
@@ -5,11 +5,13 @@ import edu.java.bot.service.update_processor.AllUpdateProcessorService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cache.CacheManager;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -20,9 +22,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @DirtiesContext
+@TestPropertySource(locations = "classpath:application-test.yml", properties = "spring.config.location=classpath:application-test.yml")
 public class BucketLimitControllerTest {
 
     public static final String UPDATES_URL = "/updates";
+    public static final int CAPACITY = 5;
     private static String LINK_UPDATE = """
         {
           "id": 1,
@@ -49,7 +53,7 @@ public class BucketLimitControllerTest {
 
     @Test
     public void bucketLimitChatControllerTest() throws Exception {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < CAPACITY; i++) {
             mockMvc.perform(post(UPDATES_URL)
                     .contentType(MediaType.APPLICATION_JSON)
                     .content(LINK_UPDATE))
